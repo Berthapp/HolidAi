c35bef4ab84916fdc28d148aeb6b6df5e4dfd2ba
WeApps <77499115+Berthapp@users.noreply.github.com>
2026-02-06 18:47:21 +0100

    Allow recaptcha in CSP

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,12 @@ import type { NextConfig } from "next";
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google.com/recaptcha/ https://www.gstatic.com/recaptcha/;
   style-src 'self' 'unsafe-inline';
   img-src 'self' data: https: blob:;
   font-src 'self' data: https:;
-  connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com;
+  connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com/recaptcha/;
+  frame-src https://www.google.com/recaptcha/;
   frame-ancestors 'none';
   base-uri 'self';
   form-action 'self';
